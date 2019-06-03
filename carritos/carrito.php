75ce568aeacd1d49129fa84edadd59784560c694
merdec <50850074+merdec@users.noreply.github.com>
2019-06-02 23:20:01 -0500

    Add files via upload

---
+++ carritos/carrito.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=divice-width,initial-scale=1">
+	<title>CARRITO</title>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+    
+	
+		<div class="panel panel-primary">
+						<div class="panel-heading">
+						<h3 class="text-center"><strong>Inicio de Sesion</strong></h3>
+					</div>
+						<div class="panel-body">
+							<form action="inicio.php" method="POST" accept-charset="utf-8" >
+								<h4>Usuario: NICELI MACHCO COCHACHIN</h4>
+								<a href="productos.php">Agregar articulo</a>
+								
+								<a href="productos.php">Cerrar sesion</a>
+								<h3>CARRITO VACIO</h3>
+
+
+							</form>
+<script src="bootstrap/js/jquery-1.12.4.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>	
+
+
+
+</body>
+</html>
